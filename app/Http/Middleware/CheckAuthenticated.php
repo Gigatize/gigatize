059660d0bfdc5f0e341cec877a231fe5f8ed1603
jrichmo93 <josh.richmond93@gmail.com>
2018-03-01 15:25:26 -0600

    saml bug fixes

---
+++ app/Http/Middleware/CheckAuthenticated.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Aacotroneo\Saml2\Facades\Saml2Auth;
+use Illuminate\Support\Facades\Auth;
+
+class CheckAuthenticated
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (Auth::guest())
+        {
+            if ($request->ajax())
+            {
+                return response('Unauthorized.', 401);
+            }
+            else
+            {
+                return Saml2Auth::login();
+                //return redirect()->guest('auth/login');
+            }
+        }
+
+        return $next($request);
+    }
+}
