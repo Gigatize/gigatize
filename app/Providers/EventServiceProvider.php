1d5b83736990c60bd427f75f30757c1d18aa35ff
jrichmo93 <josh.richmond93@gmail.com>
2018-03-01 16:05:36 -0600

    saml bug fixes

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -29,7 +29,7 @@ class EventServiceProvider extends ServiceProvider
     {
         parent::boot();
 
-        Event::listen('Aacotroneo\Saml2\Events\Saml2LoginEvent', function (Saml2LoginEvent $event) {
+        Event::listen('Aacotroneo\Saml2\Events\Saml2LoginEvent', function (\Aacotroneo\Saml2\Events\Saml2LoginEvent $event) {
             $messageId = $event->getSaml2Auth()->getLastMessageId();
             // your own code preventing reuse of a $messageId to stop replay attacks
             $user = $event->getSaml2User();
