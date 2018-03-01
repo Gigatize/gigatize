00170c4646a2606e77687bc790b058537a3f60c8
jrichmo93 <josh.richmond93@gmail.com>
2018-03-01 10:49:54 -0600

    saml bug fixes

--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-    <select class="ui fluid search dropdown" multiple="">
+    <select class="ui fluid search" multiple="">
         <option value="">State</option>
         @foreach($skills as $skill)
         <option value="{{$skill->name}}">{{$skill->name}}</option>
