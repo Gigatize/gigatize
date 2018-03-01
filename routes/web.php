f97d094e9b2198bdbd808296b17eba06a5c1bd68
jrichmo93 <josh.richmond93@gmail.com>
2018-02-28 23:10:56 -0600

    saml bug fixes

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,20 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Event::listen('Aacotroneo\Saml2\Events\Saml2LoginEvent', function (\Aacotroneo\Saml2\Events\Saml2LoginEvent $event) {
+    $messageId = $event->getSaml2Auth()->getLastMessageId();
+    // your own code preventing reuse of a $messageId to stop replay attacks
+    $user = $event->getSaml2User();
+    $userData = [
+        'id' => $user->getUserId(),
+        'attributes' => $user->getAttributes(),
+        'assertion' => $user->getRawSamlAssertion()
+    ];
+
+    dd($userData);
+    $laravelUser = //find user by ID or attribute
+        //if it does not exist create it and go on or show an error message
+        Auth::login($laravelUser);
+});
+
