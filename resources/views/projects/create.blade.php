c8052a2476798776b128c8be5dda3bba55713884
jrichmo93 <josh.richmond93@gmail.com>
2018-03-01 10:05:23 -0600

    saml bug fixes

--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -1,30 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="ui multiple dropdown">
-        <input type="hidden" name="filters">
-        <i class="filter icon"></i>
-        <span class="text">Select Skills</span>
-        <div class="menu">
-            <div class="ui icon search input">
-                <i class="search icon"></i>
-                <input type="text" placeholder="Search skills...">
-            </div>
-            <div class="divider"></div>
-            <div class="header">
-                <i class="tags icon"></i>
-                Tag Label
-            </div>
-            <div class="scrolling menu">
-                @foreach(\App\Models\Skill::all() as $skill)
-                <div class="item" data-value="important">
-                    <div class="ui red empty circular label"></div>
-                    {{$skill->name}}
-                </div>
-                @endforeach
-            </div>
-        </div>
-    </div>
+    <select class="ui fluid search dropdown" multiple="">
+        <option value="">State</option>
+        @foreach(\App\Models\Skill::all() as $skill)
+        <option value="{{$skill->name}}">{{$skill->name}}</option>
+        @endforeach
+    </select>
     <script>
         $('.ui.dropdown').dropdown();
     </script>
