22bede0a0a786a943f4dfcedf48baf94bc9e8388
jrichmo93 <josh.richmond93@gmail.com>
2018-03-01 18:31:30 -0600

    saml bug fixes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -17,21 +17,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Event::listen('Aacotroneo\Saml2\Events\Saml2LoginEvent', function (Saml2LoginEvent $event) {
-            $messageId = $event->getSaml2Auth()->getLastMessageId();
-            // your own code preventing reuse of a $messageId to stop replay attacks
-            $user = $event->getSaml2User();
-            $userData = [
-                'id' => $user->getUserId(),
-                'attributes' => $user->getAttributes(),
-                'assertion' => $user->getRawSamlAssertion()
-            ];
-
-            dd($userData);
-            $laravelUser = User::create(['name' => 'Josh', 'email' => 'josh@gigatize.io']);
-                //if it does not exist create it and go on  or show an error message
-            Auth::login($laravelUser);
-        });
+        
     }
 
     /**
