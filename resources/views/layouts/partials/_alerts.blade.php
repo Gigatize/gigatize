4f38099f8726198e7f9abc2a099f0b4deb522659
jrichmo93 <josh.richmond93@gmail.com>
2018-03-01 23:17:51 -0600

    layout updates

--- resources/views/layouts/partials/_alerts.blade.php
+++ resources/views/layouts/partials/_alerts.blade.php
@@ -1,69 +1,71 @@
-@if(isset($success))
-    <div class="ui success message">
-        <i class="close icon"></i>
-        {{$success}}
-    </div>
-@endif
+<div class="ui center aligned grid container">
+    @if(isset($success))
+        <div class="ui success message">
+            <i class="close icon"></i>
+            {{$success}}
+        </div>
+    @endif
 
-@if(isset($info))
+    @if(isset($info))
+        <div class="ui info message">
+            <i class="close icon"></i>
+            {{$info}}
+        </div>
+    @endif
+
+    @if(isset($warning))
+        <div class="ui info warning">
+            <i class="close icon"></i>
+            {{$warning}}
+        </div>
+    @endif
+
+    @if(isset($error))
+        <div class="ui info error">
+            <i class="close icon"></i>
+            {{$error}}
+        </div>
+    @endif
+
+    @if(Session::has('success'))
+        <div class="ui success message">
+            <i class="close icon"></i>
+            {{Session::get('success')}}
+        </div>
+    @endif
+
+    @if(Session::has('info'))
     <div class="ui info message">
         <i class="close icon"></i>
-        {{$info}}
+        {{Session::get('info')}}
     </div>
-@endif
+    @endif
 
-@if(isset($warning))
-    <div class="ui info warning">
-        <i class="close icon"></i>
-        {{$warning}}
-    </div>
-@endif
-
-@if(isset($error))
-    <div class="ui info error">
-        <i class="close icon"></i>
-        {{$error}}
-    </div>
-@endif
-
-@if(Session::has('success'))
-    <div class="ui success message">
-        <i class="close icon"></i>
-        {{Session::get('success')}}
-    </div>
-@endif
-
-@if(Session::has('info'))
-<div class="ui info message">
-    <i class="close icon"></i>
-    {{Session::get('info')}}
-</div>
-@endif
-
-@if(Session::has('warning'))
-    <div class="ui info warning">
-        <i class="close icon"></i>
-        {{Session::get('warning')}}
-    </div>
-@endif
-
-@if(Session::has('error'))
-    <div class="ui info error">
-        <i class="close icon"></i>
-        {{Session::get('error')}}
-    </div>
-@endif
-
-@if(Session::has('errors'))
-    <div class="ui error message">
-        <i class="close icon"></i>
-        <div class="header">
-            There were some errors with your submission
+    @if(Session::has('warning'))
+        <div class="ui info warning">
+            <i class="close icon"></i>
+            {{Session::get('warning')}}
         </div>
-        <ul class="list">
-            @foreach(Session::get('errors') as $error)
-                <li>{{$error}}</li>
-            @endforeach
-        </ul>
-    </div>
-@endif
+    @endif
+
+    @if(Session::has('error'))
+        <div class="ui info error">
+            <i class="close icon"></i>
+            {{Session::get('error')}}
+        </div>
+    @endif
+
+    @if(Session::has('errors'))
+        <div class="ui error message">
+            <i class="close icon"></i>
+            <div class="header">
+                There were some errors with your submission
+            </div>
+            <ul class="list">
+                @foreach(Session::get('errors') as $error)
+                    <li>{{$error}}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+</div>
