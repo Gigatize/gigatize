ee55bf730be4092a82fd442da792fd26ff815a56
jrichmo93 <josh.richmond93@gmail.com>
2018-03-04 20:06:56 -0600

    updated cache config

--- config/queue.php
+++ config/queue.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('QUEUE_DRIVER', 'sync'),
+    'default' => env('QUEUE_DRIVER', 'redis'),
 
     /*
     |--------------------------------------------------------------------------
@@ -79,7 +79,7 @@ return [
     */
 
     'failed' => [
-        'database' => env('DB_CONNECTION', 'mysql'),
+        'database' => 'redis',
         'table' => 'failed_jobs',
     ],
 
