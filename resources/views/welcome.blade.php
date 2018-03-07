4edc8d697660fc692ef83f0d7408d0f2518e6405
jrichmo93 <josh.richmond93@gmail.com>
2018-03-07 17:40:53 -0600

    updated home

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,13 +12,20 @@
       background: #000;
       opacity: .4;
     }
+    .yellow-text{
+      color: #FCD307;
+    }
+    .grey-text{
+      color: #717070;
+    }
   </style>
 @endsection
 
 @section('content')
   <div id="header" class="ui grid">
     <div class="ten wide column">
-        
+        <h1>LET'S <br> <span class="yellow-text">TRANSFORM</span> <br>THE WAY<br>WE WORK</h1>
+        <h4 class="grey-text">Find help when you need it, <br>help others when you can.</h4>
     </div>
     <div id="callToAction" class="six wide column">
         
