d126dddb116d317a1b05c5ab2a20d87ba0c67460
jrichmo93 <josh.richmond93@gmail.com>
2018-03-07 17:42:01 -0600

    updated home

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,8 +9,8 @@
       min-height: 500px;
     }
     #callToAction{
-      background: #000;
-      opacity: .4;
+      background: #FFF;
+      opacity: .6;
     }
     .yellow-text{
       color: #FCD307;
@@ -24,11 +24,11 @@
 @section('content')
   <div id="header" class="ui grid">
     <div class="ten wide column">
-        <h1>LET'S <br> <span class="yellow-text">TRANSFORM</span> <br>THE WAY<br>WE WORK</h1>
-        <h4 class="grey-text">Find help when you need it, <br>help others when you can.</h4>
+        
     </div>
     <div id="callToAction" class="six wide column">
-        
+        <h1>LET'S <br> <span class="yellow-text">TRANSFORM</span> <br>THE WAY<br>WE WORK</h1>
+        <h4 class="grey-text">Find help when you need it, <br>help others when you can.</h4>
     </div>
   </div>
 @endsection
