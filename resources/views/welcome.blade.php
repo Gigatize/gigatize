1e2df355d707c51b6a669f62c5139eb09794a907
jrichmo93 <josh.richmond93@gmail.com>
2018-03-07 17:47:23 -0600

    updated home

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -27,7 +27,7 @@
         
     </div>
     <div id="callToAction" class="six wide column">
-      <div class="middle aligned">
+      <div class="middle aligned" style="height: 100%">
         <h1>LET'S <br> <span class="yellow-text">TRANSFORM</span> <br>THE WAY<br>WE WORK</h1>
         <h4 class="grey-text">Find help when you need it, <br>help others when you can.</h4>
       </div>
