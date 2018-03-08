2f1996636863e4593a2fe50d3c9b7da86e0b057e
jrichmo93 <josh.richmond93@gmail.com>
2018-03-07 21:56:25 -0600

    updated home

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -103,7 +103,7 @@
   </div>
   <div class="ui grid centered container">
     <div class="three wide column yellow tile">
-      <button class="ui grey basic button bottom aligned fluid">Grey</button>
+      <button class="ui grey basic button bottom aligned fluid">Read More</button>
     </div>
     <div class="three wide column tile"></div>
     <div class="three wide column yellow tile"></div>
