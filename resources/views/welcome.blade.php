cf6619c400c2d6beda72ff36f068fbaff6da3307
jrichmo93 <josh.richmond93@gmail.com>
2018-03-07 18:33:50 -0600

    updated home

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -45,8 +45,8 @@
         <h1>LET'S <br> <span class="yellow-text">TRANSFORM</span> <br>THE WAY<br>WE WORK</h1>
         <h4 class="grey-text middle aligned">Find help when you need it, <br>help others when you can.</h4>
         <br>
-        <button class="huge ui yellow basic button no-radius" style="color: #000!important">FIND A GIG</button>
-        <button class="huge ui yellow basic button no-radius" style="color: #000!important">POST A GIG</button>
+        <button class="big ui yellow basic button no-radius" style="color: #000!important">FIND A GIG</button>
+        <button class="big ui yellow basic button no-radius" style="color: #000!important">POST A GIG</button>
       </div>
     </div>
   </div>
