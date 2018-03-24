ccf03f04eaa0538f2a747b0a297e350661b3938f
jrichmo93 <josh.richmond93@gmail.com>
2018-03-24 15:51:37 -0500

    updated create project layout

--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -309,10 +309,10 @@
                         <label>ACCEPTANCE CRITERIA</label>
                         <div class="header two fields">
                             <div id="criteria_wrapper" class="thirteen wide field">
-                                <input type="text" id="criteria_input" placeholder="Criteria...">
+                                <input type="text" id="criteria_input" placeholder="Enter Criteria...">
                             </div>
                             <div class="three wide field">
-                                <div class="ui button green" tabindex="0" onclick="newElement()" style="width: 125px;">Add</div>
+                                <div class="ui button green" tabindex="0" onclick="newElement()" style="width: 100px;">Add</div>
                             </div>
                         </div>
 
@@ -362,30 +362,19 @@
                 //activate next step on progressbar using the index of next_fs
                 $("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
                 //show the next fieldset
-                next_fs.show();
                 //hide the current fieldset with style
                 current_fs.animate({opacity: 0}, {
                     step: function (now, mx) {
-                        //as the opacity of current_fs reduces to 0 - stored in "now"
-                        //1. scale current_fs down to 80%
-                        scale = 1 - (1 - now) * 0.2;
-                        //2. bring next_fs from the right(50%)
-                        left = (now * 50) + "%";
-                        //3. increase opacity of next_fs to 1 as it moves in
+                        //increase opacity of next_fs to 1 as it moves in
                         opacity = 1 - now;
-                        current_fs.css({
-                            'transform': 'scale(' + scale + ')',
-                            'position': 'absolute'
-                        });
-                        next_fs.css({'left': left, 'opacity': opacity});
+                        next_fs.css({'opacity': opacity});
                     },
                     duration: 800,
                     complete: function () {
-                        current_fs.hide();
+                        current_fs.hide(500);
+                        next_fs.show(500);
                         animating = false;
                     },
-                    //this comes from the custom easing plugin
-                    easing: 'easeInOutBack'
                 });
             });
             $(".previous").click(function () {
@@ -395,29 +384,19 @@
                 previous_fs = $(this).parent().prev();
                 //de-activate current step on progressbar
                 $("#progressbar li").eq($("fieldset").index(current_fs)).removeClass("active");
-                //show the previous fieldset
-                previous_fs.show();
                 //hide the current fieldset with style
                 current_fs.animate({opacity: 0}, {
                     step: function (now, mx) {
-                        //as the opacity of current_fs reduces to 0 - stored in "now"
-                        //1. scale previous_fs from 80% to 100%
-                        scale = 0.8 + (1 - now) * 0.2;
-                        //2. take current_fs to the right(50%) - from 0%
-                        left = ((1 - now) * 50) + "%";
-                        //3. increase opacity of previous_fs to 1 as it moves in
+                        //increase opacity of previous_fs to 1 as it moves in
                         opacity = 1 - now;
-                        current_fs.css({'left': left});
-                        previous_fs.css({'transform': 'scale(' + scale + ')', 'opacity': opacity, 'position': 'absolute'});
+                        previous_fs.css({'opacity': opacity});
                     },
                     duration: 800,
                     complete: function () {
-                        current_fs.hide();
+                        current_fs.hide(500);
+                        previous_fs.show(500);
                         animating = false;
-                        previous_fs.css({'position': 'relative'});
                     },
-                    //this comes from the custom easing plugin
-                    easing: 'easeInOutBack'
                 });
 
             });
