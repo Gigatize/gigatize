262799d0bec08cb88b96f290118c199c5c6f045f
jrichmo93 <josh.richmond93@gmail.com>
2018-04-02 20:53:05 -0500

    updated find a gig page

--- resources/views/projects/project_tile.blade.php
+++ resources/views/projects/project_tile.blade.php
@@ -17,7 +17,7 @@
             <p>{{$project->description}}</p>
         @endif
     </div>
-    <div class="content" style="position: relative">
+    <div class="content like" style="position: relative">
                     <span class="right floated">
                         <span class="comment-count">5</span> <i class="fas fa-comment"></i>
                         <span class="favorite-count" data-project="{{$project->id}}" style="margin-left: 3px;">{{$project->favoriteCount()}}</span> <i class="@if($project->favorited()) red-text @endif fas fa-heart favorite" data-project="{{$project->id}}"></i>
