d2afe267bc723acc7291dda6a0a381a708abef17
afineedge <eedgerton47@gmail.com>
2018-04-28 23:30:16 -0400

    Your Profile and Company Profile HTML pages

---
+++ html_mockups/js/global-community.js
@@ -0,0 +1,318 @@
+$(document).ready(function() {
+    $.fn.dataTable.ext.classes.sPageButton = 'btn btn-sm btn-empty';
+
+    var series = [
+        ['USA', 55, 'United States'],
+        ['ITA', 10, 'Italy'],
+        ['GBR', 16, 'Great Britain'],
+        ['BRA', 11, 'Brazil'],
+        ['IND', 10, 'India'],
+        ['CAN', 8, 'Canada'],
+        ['MEX', 6, 'Mexico'],
+        ['RUS', 3, 'Russia'],
+        ['AUS', 3, 'Australia']
+    ];
+
+    $('#global_community_table').DataTable({
+        dom: 't',
+        data: series,
+        order: [[ 1, "desc" ]],
+        pageLength: 8,
+        columns: [
+            {
+                data: [2]
+            },{
+                data: [1],
+                "render": function ( data, type, row, meta ) {
+                    return data + '%';
+                }
+            }
+        ]
+    });
+
+    var dataset = {};
+    // We need to colorize every country based on "numberOfWhatever"
+    // colors should be uniq for every value.
+    // For this purpose we create palette(using min/max series-value)
+    var onlyValues = series.map(function(obj){ return obj[1]; });
+    var minValue = Math.min.apply(null, onlyValues),
+            maxValue = Math.max.apply(null, onlyValues);
+    // create color palette function
+    // color can be whatever you wish
+    var paletteScale = d3.scale.linear()
+            .domain([minValue,maxValue])
+            .range(["#ffea94","#FFD11F"]); // blue color
+    // fill dataset in appropriate format
+    series.forEach(function(item){ //
+        // item example value ["USA", 70]
+        var iso = item[0],
+                value = item[1];
+        dataset[iso] = { numberOfThings: value, fillColor: paletteScale(value) };
+    });
+
+    var global_community = new Datamap({
+        element: document.getElementById("global_community"),
+        projection: 'mercator',
+        height: null,
+        width: null,
+        setProjection: function (element) {
+            var projection = d3.geo.mercator().center([10, 0]).scale(67).translate([element.offsetWidth / 2, element.offsetHeight / 2 + element.offsetHeight / 4]);
+            var path = d3.geo.path().projection(projection);
+            return { path: path, projection: projection };
+        },
+        responsive: true,
+        fills: { defaultFill: '#fff7d6' },
+        data: dataset,
+        geographyConfig: {
+            borderColor: '#DEDEDE',
+            borderWidth: 0,
+            highlightBorderWidth: 1,
+            // don't change color on mouse hover
+            highlightFillColor: function(geo) {
+                return geo['fillColor'] || '#fff7d6';
+            },
+            // only change border
+            highlightBorderColor: '#d1a700',
+            // show desired information in tooltip
+            popupTemplate: function(geo, data) {
+                // don't show tooltip if country don't present in dataset
+                if (!data) { return ; }
+                // tooltip content
+                return ['<div class="hoverinfo">',
+                    '<strong>', geo.properties.name, '</strong>',
+                    '<br>Count: <strong>', data.numberOfThings, '</strong>',
+                    '</div>'].join('');
+            }
+        }
+    });
+
+    var colors = d3.scale.category10();
+
+    window.addEventListener('resize', function(event){
+        global_community.resize();
+    });
+
+    var team_members = [{
+        id: 'AP1',
+        photo: 'http://placehold.it/200x200/',
+        name: 'Ayushi Patidar',
+        impact_score: 74,
+        impact_score_change: 1
+    },{
+        id: 'CH1',
+        photo: 'http://placehold.it/200x200/',
+        name: 'Camden Hill',
+        impact_score: 79,
+        impact_score_change: 6
+    },{
+        id: 'TP1',
+        photo: 'http://placehold.it/200x200/',
+        name: 'Tobi Polland',
+        impact_score: 85,
+        impact_score_change: 10
+    },{
+        id: 'JR1',
+        photo: 'http://placehold.it/200x200/',
+        name: 'Josh Richmond',
+        impact_score: 90,
+        impact_score_change: 1
+    },{
+        id: 'EE1',
+        photo: 'http://placehold.it/200x200/',
+        name: 'Eric Edgerton',
+        impact_score: 70,
+        impact_score_change: 10
+    }];
+
+    $('#top_gigatizers_table').DataTable({
+        dom: 't',
+        data: team_members,
+        order: [[ 0, "desc" ]],
+        pageLength: 4,
+        autoWidth: false,
+        columns: [
+            {
+                data: 'impact_score',
+                visible: false
+            },{
+                data: 'photo',
+                className: 'photo-thumbnail-container',
+                "render": function ( data, type, row, meta ) {
+                    return '<div class="photo-thumbnail"><img src="' + data + '" /></div>'
+                }
+            },{
+                defaultContent: '',
+                width: '100%',
+                "render": function ( data, type, row, meta ) {
+                    var info = $('<div>');
+                    info.append($('<small>', {
+                        html: '<strong>' + row.name + '</strong><br />'
+                    }));
+                    info.append($('<small>', {
+                        html: row.impact_score + ' Impact Score<br />'
+                    }));
+
+                    var impact_score_change = row.impact_score_change;
+                    if (impact_score_change > -1){
+                        impact_score_change = '+' + impact_score_change;
+                    }
+
+                    info.append($('<small>', {
+                        html: impact_score_change + ' Today<br />'
+                    }));
+                    return info.html();
+                }
+            }
+        ]
+    });
+
+    var gigs = [{
+        id: 'brilliant',
+        name: 'Brilliant Design Dashboard',
+        teamMembers: ['TP1', 'JR1'],
+        created: 'May 5, 2018',
+        progress: 70,
+        status: 3
+    },{
+        id: 'shawneeMacro',
+        name: 'VBA Macro for Shawnee',
+        teamMembers: ['CH1', 'AP1'],
+        created: 'May 3, 2018',
+        progress: 90,
+        status: 3
+    },{
+        id: 'imax',
+        name: 'IMAX Logo Design',
+        teamMembers: ['TP1', 'EE1'],
+        created: 'May 3, 2018',
+        progress: 60,
+        status: 3
+    },{
+        id: 'tableauDashboard',
+        name: 'Tableau Dashboard for HQ',
+        teamMembers: ['TP1', 'JR1'],
+        created: 'May 2, 2018',
+        progress: 60,
+        status: 1
+    },{
+        id: 'haz',
+        name: 'Hazardous Waste Scanning',
+        teamMembers: ['TP1', 'JR1'],
+        created: 'May 7, 2018',
+        progress: 12,
+        status: 2
+    }];
+
+    $('#this_months_gigs_table').DataTable({
+        dom: 'rt',
+        data: gigs,
+        order: [[ 0, "desc" ]],
+        pageLength: 5,
+        autoWidth: false,
+        scrollY: '400px',
+        scrollCollapse: true,
+        columns: [
+            {
+                data: 'id',
+                visible: false
+            },{
+                data: 'name',
+                "render": function ( data, type, row, meta ) {
+                    return data + '<br /><small>Created ' + row.created + '</small>'
+                }
+            },{
+                data: 'teamMembers',
+                "render": function ( data, type, row, meta ) {
+                    var photoContainer = $('<div />');
+                    for (var i = 0; i < data.length; i++){
+                        var photoThumbnail = $('<div />', {
+                            class: 'photo-thumbnail photo-thumbnail-small photo-thumbnail-square',
+                            style: 'display: inline-block; margin-right: 4px;'
+                        });
+                        var photo = $('<img />');
+                        var teamMember = team_members.filter(function(member){
+                            return member.id == data[i];
+                        });
+                        photo[0].src = teamMember[0].photo;
+                        photo[0].title = teamMember[0].name;
+                        photoContainer.append(photoThumbnail.append(photo));
+                    }
+                    return photoContainer.html();
+                }
+            },{
+                data: 'progress',
+                "render": function ( data, type, row, meta ) {
+                    var progressContainer = $('<div />');
+                    var progressWrapper = $('<div />', {
+                        class: 'progress'
+                    });
+                    var progressBar = $('<div />', {
+                        class: 'progress-bar',
+                        role: 'progressbar',
+                        width: data + '%'
+                    });
+
+                    progressWrapper.append(progressBar);
+                    progressContainer.append(progressWrapper);
+                    progressContainer.append('<small>' + data + '% Complete</small>');
+                    return progressContainer.html();
+                }
+            },{
+                data: 'status',
+                "render": function ( data, type, row, meta ) {
+                    var statusContainer = $('<div />');
+                    var statusBtn = $('<div />', {
+                        class: 'badge badge-pill full-width p-2'
+                    });
+
+                    if (data == '3'){
+                        statusBtn.addClass('badge-success');
+                        statusBtn.html('SUCCESS');
+                    } else if (data == '2'){
+                        statusBtn.addClass('badge-warning');
+                        statusBtn.html('HOLD');
+                    } else if (data == '1'){                        statusBtn.addClass('badge-danger');
+                        statusBtn.html('BLOCKED');
+                    }
+                    statusContainer.append(statusBtn);
+                    return statusContainer.html();
+                }
+            },{
+                defaultContent: '',
+                width: '120px',
+                "render": function ( data, type, row, meta ) {
+                    var actionsContainer = $('<div />');
+                    var buttonGroup = $('<div />', {
+                        class: 'btn-group btn-group-sm'
+                    });
+                    var viewBtn = $('<btn />', {
+                        class: 'btn',
+                        title: 'View',
+                        html: '<i class="fas fa-eye"></i>'
+                    });
+                    buttonGroup.append(viewBtn);
+                    var upvoteBtn = $('<btn />', {
+                        class: 'btn',
+                        title: 'Upvote',
+                        html: '<i class="fas fa-thumbs-up"></i>'
+                    });
+                    buttonGroup.append(upvoteBtn);
+                    var commentBtn = $('<btn />', {
+                        class: 'btn',
+                        title: 'Comment',
+                        html: '<i class="fas fa-comments"></i>'
+                    });
+                    buttonGroup.append(commentBtn);
+                    var favoriteBtn = $('<btn />', {
+                        class: 'btn',
+                        title: 'Add to Favorites',
+                        html: '<i class="fas fa-star"></i>'
+                    });
+                    buttonGroup.append(favoriteBtn);
+                    actionsContainer.append(buttonGroup);
+                    return actionsContainer.html();
+                }
+            }
+        ]
+    });
+});
---
+++ html_mockups/js/your-profile.js
@@ -0,0 +1,201 @@
+$(document).ready(function() {
+    $.fn.dataTable.ext.classes.sPageButton = 'btn btn-sm btn-empty';
+
+    var team_members = [{
+        id: 'AP1',
+        photo: 'http://placehold.it/200x200/',
+        name: 'Ayushi Patidar',
+        impact_score: 74,
+        impact_score_change: 1
+    },{
+        id: 'CH1',
+        photo: 'http://placehold.it/200x200/',
+        name: 'Camden Hill',
+        impact_score: 79,
+        impact_score_change: 6
+    },{
+        id: 'TP1',
+        photo: 'http://placehold.it/200x200/',
+        name: 'Tobi Polland',
+        impact_score: 85,
+        impact_score_change: 10
+    },{
+        id: 'JR1',
+        photo: 'http://placehold.it/200x200/',
+        name: 'Josh Richmond',
+        impact_score: 90,
+        impact_score_change: 1
+    },{
+        id: 'EE1',
+        photo: 'http://placehold.it/200x200/',
+        name: 'Eric Edgerton',
+        impact_score: 70,
+        impact_score_change: 10
+    }];
+
+    var gigs = [{
+        id: 'brilliant',
+        name: 'Brilliant Design Dashboard',
+        teamMembers: ['TP1', 'JR1'],
+        created: 'May 5, 2018',
+        progress: 70,
+        status: 3,
+        hoursSpent: 9,
+        review: 5
+    },{
+        id: 'imax',
+        name: 'IMAX Logo Design',
+        teamMembers: ['TP1', 'EE1'],
+        created: 'May 3, 2018',
+        progress: 60,
+        status: 3,
+        hoursSpent: 5,
+        review: 4.5
+    },{
+        id: 'tableauDashboard',
+        name: 'Tableau Dashboard for HQ',
+        teamMembers: ['TP1', 'JR1'],
+        created: 'May 2, 2018',
+        progress: 60,
+        status: 1,
+        hoursSpent: 1,
+        review: 4
+    },{
+        id: 'haz',
+        name: 'Hazardous Waste Scanning',
+        teamMembers: ['TP1', 'JR1'],
+        created: 'May 7, 2018',
+        progress: 12,
+        status: 2,
+        hoursSpent: 2,
+        review: 5
+    }];
+
+    $('#gig_portfolio_table').DataTable({
+        dom: 'rt',
+        data: gigs,
+        order: [[ 0, "desc" ]],
+        pageLength: 5,
+        autoWidth: false,
+        columns: [
+            {
+                title: 'ID',
+                data: 'id',
+                visible: false
+            },{
+                title: 'Gig Name',
+                data: 'name',
+                "render": function ( data, type, row, meta ) {
+                    return data + '<br /><small>Created ' + row.created + '</small>'
+                }
+            },{
+           /*     data: 'teamMembers',
+                "render": function ( data, type, row, meta ) {
+                    var photoContainer = $('<div />');
+                    var photoThumbnail = $('<div />', {
+                        class: 'photo-thumbnail photo-thumbnail-small photo-thumbnail-square',
+                        style: 'display: inline-block; margin-right: 4px;'
+                    });
+                    var photo = $('<img />');
+                    var teamMember = team_members.filter(function(member){
+                        return member.id == data[0];
+                    });
+                    photo[0].src = teamMember[0].photo;
+                    photo[0].title = teamMember[0].name;
+                    photoContainer.append(photoThumbnail.append(photo));
+                    return photoContainer.html();
+                }
+            },{*/
+                title: 'Hours<br />Spent',
+                data: 'hoursSpent',
+                class: 'text-center'
+            },{
+                title: 'Team<br />Members',
+                data: 'teamMembers',
+                "render": function ( data, type, row, meta ) {
+                    var photoContainer = $('<div />');
+                    for (var i = 0; i < data.length; i++){
+                        var photoThumbnail = $('<div />', {
+                            class: 'photo-thumbnail photo-thumbnail-small photo-thumbnail-square',
+                            style: 'display: inline-block; margin-right: 4px;'
+                        });
+                        var photo = $('<img />');
+                        var teamMember = team_members.filter(function(member){
+                            return member.id == data[i];
+                        });
+                        photo[0].src = teamMember[0].photo;
+                        photo[0].title = teamMember[0].name;
+                        photoContainer.append(photoThumbnail.append(photo));
+                    }
+                    return photoContainer.html();
+                }
+            },{
+                title: 'Gig Progress',
+                data: 'progress',
+                "render": function ( data, type, row, meta ) {
+                    var progressContainer = $('<div />');
+                    var progressWrapper = $('<div />', {
+                        class: 'progress'
+                    });
+                    var progressBar = $('<div />', {
+                        class: 'progress-bar',
+                        role: 'progressbar',
+                        width: data + '%'
+                    });
+
+                    progressWrapper.append(progressBar);
+                    progressContainer.append(progressWrapper);
+                    progressContainer.append('<small>' + data + '% Complete</small>');
+                    return progressContainer.html();
+                }
+            },{
+                title: 'Status',
+                data: 'status',
+                "render": function ( data, type, row, meta ) {
+                    var statusContainer = $('<div />');
+                    var statusBtn = $('<div />', {
+                        class: 'badge badge-pill full-width p-2'
+                    });
+
+                    if (data == '3'){
+                        statusBtn.addClass('badge-success');
+                        statusBtn.html('SUCCESS');
+                    } else if (data == '2'){
+                        statusBtn.addClass('badge-warning');
+                        statusBtn.html('HOLD');
+                    } else if (data == '1'){                        statusBtn.addClass('badge-danger');
+                        statusBtn.html('BLOCKED');
+                    }
+                    statusContainer.append(statusBtn);
+                    return statusContainer.html();
+                }
+            },{
+                title: 'Review',
+                data: 'review',
+                width: '80px',
+                "render": function ( data, type, row, meta ) {
+                    var reviewContainer = $('<div />');
+                    var fullStars = 0;
+                    if (data % 1 !== 0){
+                        fullStars = data - .5;
+                    }
+                    for (var i = 0; i < data; i++){
+                        var star = $('<i />', {
+                            class: 'fas fa-star',
+                            style: 'color: #FFD11F;'
+                        });
+                        reviewContainer.append(star);
+                    }
+                    for (var i = 5; i > data; i--){
+                        var starGray = $('<i />', {
+                            class: 'fas fa-star',
+                            style: 'color: #eee;'
+                        });
+                        reviewContainer.append(star);
+                    }
+                    return reviewContainer.html();
+                }
+            }
+        ]
+    });
+});
