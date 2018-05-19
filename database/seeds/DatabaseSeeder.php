42ce70d44e15f61d74be4c4ec4b60196b21ed2ca
jrichmo93 <josh.richmond93@gmail.com>
2018-05-19 13:48:12 -0500

    updated seeds

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            CategoriesTableSeeder::class,
+            //CategoriesTableSeeder::class,
             SkillsTableSeeder::class,
             LocationsTableSeeder::class,
         ]);
